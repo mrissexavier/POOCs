b02b8c9c8f963553dd0c815590372b4314e6ba6f
Emily Xavier <m.risse.dev@gmail.com>
2022-03-31 10:49:43 -0300

    Classe Pessoa

---
+++ Ex01/Ex01/Pessoa.cs
@@ -0,0 +1,11 @@
+﻿namespace Ex01
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private int idade;
+
+        public int Idade { get => idade; set => idade = value; }
+        public string Nome { get => nome; set => nome = value; }
+    }
+}
---
+++ Ex01/Ex01/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Ex01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Pessoa pessoa1, pessoa2;
+            pessoa1 = new Pessoa();
+            pessoa2 = new Pessoa();
+
+            pessoa1.Nome = "Emily";
+            pessoa1.Idade = 17;
+            pessoa2.Nome = "João";
+            pessoa2.Idade = 16;
+
+
+           /* Console.WriteLine("Digite o nome da primeira pessoa: ");
+            pessoa1.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a Idade da primeira pessoa: ");
+            pessoa1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome da segunda pessoa: ");
+            pessoa2.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a Idade da segunda pessoa: ");
+            pessoa2.Idade = int.Parse(Console.ReadLine());
+
+            Console.ReadKey();
+            Console.Clear(); */
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.WriteLine($"Nome: {pessoa1.Nome}");
+            Console.WriteLine($"Idade: {pessoa1.Idade}");
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.WriteLine($"Nome: {pessoa2.Nome}");
+            Console.WriteLine($"Idade: {pessoa2.Idade}");
+
+            if (pessoa1.Idade > pessoa2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + pessoa1.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + pessoa2.Nome);
+            }
+        }
+    }
+}
