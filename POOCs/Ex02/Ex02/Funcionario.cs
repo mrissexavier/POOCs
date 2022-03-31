cdacdcf9ec3ae4c6c11cf45c85853584f8c92671
Emily Xavier <m.risse.dev@gmail.com>
2022-03-31 11:15:05 -0300

    Classe funcionario

---
+++ POOCs/Ex02/Ex02/Funcionario.cs
@@ -0,0 +1,11 @@
+﻿namespace Ex02
+{
+    internal class Funcionario
+    {
+        private string nome;
+        private double salario;
+
+        public string Nome { get => nome; set => nome = value; }
+        public double Salario { get => salario; set => salario = value; }
+    }
+}
---
+++ POOCs/Ex02/Ex02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario1 = new Funcionario();
+            Funcionario funcionario2 = new Funcionario();
+
+            funcionario1.Nome = "Carlos Silva";
+            funcionario1.Salario = 6300.00;
+            funcionario2.Nome = "Ana Marques";
+            funcionario2.Salario = 6700.00;
+
+            double salarioMedio = (funcionario1.Salario + funcionario2.Salario) / 2.0;
+
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.WriteLine($"Nome: {funcionario1.Nome}");
+            Console.WriteLine("Salário: R$" + funcionario1.Salario.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.WriteLine($"Nome: {funcionario2.Nome}");
+            Console.WriteLine("Salário: R$" + funcionario2.Salario.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Salário médio = R$" + salarioMedio.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
