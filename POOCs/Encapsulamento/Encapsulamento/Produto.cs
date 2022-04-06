c039195a83f9dbbc24bda2d06b362a49ba6ce273
Emily Xavier <m.risse.dev@gmail.com>
2022-04-06 09:56:50 -0300

    Getter e Setter C#

---
+++ POOCs/Encapsulamento/Encapsulamento/Produto.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+namespace Construtor
+{
+    class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public string Nome { get => _nome; set => _nome = value; }   
+        public double Preco { get => _preco; }
+        public int Quantidade { get => _quantidade; }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco)
+        {
+            _nome = nome;
+            _preco = preco;
+        }
+
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + _quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ POOCs/Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,20 @@
+﻿using Construtor;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto("TV",  500.00, 10);
+
+            produto.Nome = "TV 4K";
+
+            Console.WriteLine(produto.Nome);
+            Console.WriteLine(produto);
+
+            
+        }
+    }
+}
