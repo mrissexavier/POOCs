4132c18862f02415d5d8dbac69e53a1053401cd4
Emily Xavier <m.risse.dev@gmail.com>
2022-04-06 22:01:29 -0300

    Ex03

---
+++ POOCs/Ex03/Ex03/Conta.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace Ex03
+{
+    internal class ContaBancaria
+    {
+
+        public int Conta { get; private set; }
+        public string Titular { get; private set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int conta, string titular)
+        {
+            Conta = conta;
+            Titular = titular;
+        }
+
+        public ContaBancaria(int conta, string titular, double saldo) : this(conta, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: "
+                + Conta
+                + ", Titular: "
+                + Titular
+                + ", Saldo: R$ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public double Deposito(double valorDeposito)
+        {
+            return Saldo += valorDeposito;
+        }
+
+        public double Saque(double valorSaque)
+        {
+            return Saldo -= valorSaque;
+        }
+
+        
+
+    }
+}
---
+++ POOCs/Ex03/Ex03/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Digite o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+            Console.Write("Haverá depósito? (s/n): ");
+            char simOuNao = char.Parse(Console.ReadLine());
+
+            if (simOuNao == 's' || simOuNao == 'S')
+            {
+                Console.Write("Digite o valor a ser depositado: ");
+                double valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numeroConta,  nomeTitular, valorDeposito);
+            }
+            else
+            {
+                conta = new ContaBancaria(numeroConta, nomeTitular);
+            }
+
+            Console.WriteLine("Dados da conta: \n" + conta);
+            Console.WriteLine();
+
+            Console.Write("Digite o valor para depósito: ");
+            double valorDeposito1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(valorDeposito1);
+            Console.WriteLine();
+
+
+            Console.WriteLine("Dados atualizados da conta: \n" + conta);
+            Console.WriteLine();
+
+            Console.Write("Digite o valor que deseja sacar: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+            conta.Saque(valorSaque);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados da conta: \n" + conta);
+
+        }
+    }
+}
